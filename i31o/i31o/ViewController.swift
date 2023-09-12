18bcf442597bc23cd46547ae796c86601da5b98d
romanenkoio <zamok.tech@gmail.com>
2023-09-12 19:30:53 +0300

    initial commit

---
+++ i31o/i31o/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i31o
+//
+//  Created by Romanenko Ilya on 12.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
